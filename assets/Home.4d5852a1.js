38b707690f1ec682c2eaa807f0a5da159194af43
tinnakrit <tinnkrit.n@gmail.com>
2021-12-03 00:24:16 +0700

    deploy

--- assets/Home.4d5852a1.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index.8ce044ff.js";import{o,c as r}from"./vendor.91338f32.js";const c={};function n(t,f){return o(),r("div",null,"Home")}var _=e(c,[["render",n]]);export{_ as default};

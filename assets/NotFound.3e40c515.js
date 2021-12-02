e34b4368f1e3221b7ca7766c050f25772159144e
tinnakrit <tinnkrit.n@gmail.com>
2021-12-03 00:07:27 +0700

    deploy

--- assets/NotFound.3e40c515.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index.8efb50f8.js";import{o,c as n}from"./vendor.91338f32.js";const r={};function t(c,f){return o(),n("div",null,"not found")}var _=e(r,[["render",t]]);export{_ as default};
